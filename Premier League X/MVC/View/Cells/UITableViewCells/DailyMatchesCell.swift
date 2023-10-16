89a662f39337063f611d75b0318ffa0ccaf83b7e
Wait-eg-ios <iphonewaiteg@gmail.com>
2023-10-16 19:17:05 +0300

    Solve Swift message package problem

---
+++ Premier League X/MVC/View/Cells/UITableViewCells/DailyMatchesCell.swift
@@ -0,0 +1,26 @@
+//
+//  DailyMatchesCell.swift
+//  Premier League X
+//
+//  Created by Hady on 16/10/2023.
+//
+
+import UIKit
+
+class DailyMatchesCell: UITableViewCell {
+
+    static let id = String(describing: DailyMatchesCell.self)
+    static let rowHeight: CGFloat =  100
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
